5255fd12ab42a38ed9aae3b7c10297eb3164dd6a
fgutierrezalbornoz <francisco.gutial@gmail.com>
2024-09-15 13:23:06 -0300

    aux terminada sin bonus

--- todoapp/views.py
+++ todoapp/views.py
@@ -2,12 +2,17 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from todoapp.models import Tarea
+from todoapp.models import Tarea, User
 from categorias.models import Categoria
-
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def tareas(request):  # the index view
-    mis_tareas = Tarea.objects.all()  # quering all todos with the object manager
+    if request.user.is_authenticated:
+        mis_tareas = Tarea.objects.filter(owner=request.user)# quering all todos with the object manager
+    else:
+        mis_tareas = Tarea.objects.filter(owner=None)
     categorias = Categoria.objects.all()  # getting all categories with object manager
 
     if request.method == "GET":
@@ -19,6 +24,49 @@ def tareas(request):  # the index view
             nombre_categoria = request.POST["selector_categoria"]  # nombre de la categoria
             categoria = Categoria.objects.get(nombre=nombre_categoria)  # buscar la categoría en la base de datos
             contenido = request.POST["contenido"]  # contenido de la tarea
-            nueva_tarea = Tarea(titulo=titulo, contenido=contenido, categoria=categoria)  # Crear la tarea
+
+            #Verificar si el usuario inició sesión o no!!
+            if request.user.is_authenticated:
+                nueva_tarea = Tarea(titulo=titulo, contenido=contenido, categoria=categoria,owner=request.user)  # Crear la tarea
+            else:
+                nueva_tarea = Tarea(titulo=titulo, contenido=contenido, categoria=categoria)
             nueva_tarea.save()  # guardar la tarea en la base de datos.
             return redirect("/tareas")  # recargar la página.
+        
+
+def register_user(request):
+    if request.method == 'GET': #Si estamos cargando la página
+     return render(request, "todoapp/register_user.html") #Mostrar el template
+
+    elif request.method == 'POST': #Si estamos recibiendo el form de registro
+     #Tomar los elementos del formulario que vienen en request.POST
+     nombre = request.POST['nombre']
+     contraseña = request.POST['contraseña']
+     apodo = request.POST['apodo']
+     pronombre = request.POST['pronombre']
+     mail = request.POST['mail']
+
+     #Crear el nuevo usuario
+     user = User.objects.create_user(username=nombre, password=contraseña, email=mail, apodo=apodo, pronombre=pronombre)
+     messages.success(request, 'Se creó el usuario para ' + user.apodo)
+     #Redireccionar la página /tareas
+     return HttpResponseRedirect('/tareas')
+
+
+def login_user(request):
+    if request.method == 'GET':
+        return render(request,"todoapp/login_user.html")
+    if request.method == 'POST':
+        username = request.POST['username']
+        contraseña = request.POST['contraseña']
+        usuario = authenticate(username=username,password=contraseña)
+        if usuario is not None:
+            login(request,usuario)
+            return HttpResponseRedirect('/tareas')
+        else:
+            return HttpResponseRedirect('/register')
+        
+ 
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect('/tareas')
